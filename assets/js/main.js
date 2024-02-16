678e65e85d27b3b7e2be7abc8e8a741f878987cf
douglasfcsdev <douglas.fcsdev@gmail.com>
2024-02-16 17:47:03 -0300

    pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,41 @@
-console.log('teste01')
+const pokemonsList = document.getElementById("pokemonsList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 10;
+let offset = 0;
+
+function laodPokemonitens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    pokemonsList.innerHTML += pokemons
+      .map(
+        pokemon => `
+          <li class="pokemon ${pokemon.type}">
+              <h2 class="name">${pokemon.name}</h2>
+              <span class="number">#${pokemon.order}</span>
+
+              <div class="detail">
+                  <ol class="types ">
+                  ${pokemon.types
+                    .map(
+                      type => `<li class="type ${pokemon.type}">${type}</li>`
+                    )
+                    .join("")}
+                  </ol>
+
+                  <img
+                  src=${pokemon.image}
+                  alt="${pokemon.name}"
+                  />
+            </div>
+          </li>
+      `
+      )
+      .join("");
+  });
+}
+
+laodPokemonitens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  laodPokemonitens(offset, limit);
+});
---
+++ assets/js/model/pokemon_model.js
@@ -0,0 +1,7 @@
+class Pokemon {
+  name;
+  order;
+  type;
+  types = [];
+  image;
+}
